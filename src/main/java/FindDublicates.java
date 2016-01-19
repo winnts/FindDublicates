47d8673f204d6737a422a3c1b6eaf29b74929eb8
Andrey Dyachenko <winnts@gmail.com>
2016-01-19 20:11:10 +0200

    Added multithreading

--- src/main/java/FindDublicates.java
+++ src/main/java/FindDublicates.java
@@ -9,6 +9,9 @@ import org.elasticsearch.node.Node;
 import org.elasticsearch.search.SearchHit;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.LongAdder;
 
 import static org.elasticsearch.index.query.FilterBuilders.andFilter;
 import static org.elasticsearch.index.query.FilterBuilders.termFilter;
@@ -21,6 +24,8 @@ import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 public class FindDublicates {
     static Node node;
     static Client client;
+    static LongAdder counter = new LongAdder();
+    static LongAdder deleteCounter = new LongAdder();
 
     public static SearchResponse scrollES(String[] indexName, QueryBuilder fqb, Integer size, String[] fields) {
         SearchResponse scrollResp = client.prepareSearch(indexName)
@@ -38,22 +43,22 @@ public class FindDublicates {
                 .actionGet();
     }
 
-    public void equalHostSearch(FilteredQueryBuilder searchFor, String[] index, String[] indexWhereFind) throws IOException {
-        Integer counter = 0;
-        Long deleteCounter = 0L;
+    public void equalHostSearch(FilteredQueryBuilder searchFor, String[] index, String[] indexWhereFind, Integer reader_id) throws IOException {
+//        Integer counter = 0;
+//        Long deleteCounter = 0L;
         String[] returnFields = {"domain"};
         SearchResponse scrollResp = scrollES(index, searchFor, 10, returnFields);
         System.out.println("TOTAL RECORDS: " + scrollResp.getHits().totalHits());
         while (true) {
             for (SearchHit hit : scrollResp.getHits().getHits()) {
                 String hostToFind = hit.field("domain").getValue().toString();
-                System.out.println("Searching for: " + hostToFind);
+                System.out.println("Searching for: " + hostToFind + " reader_id: " + reader_id);
                 FilteredQueryBuilder findDoc = filteredQuery(matchAllQuery(), termFilter("domain", hostToFind));
                 SearchResponse scrollResp2 = scrollES(indexWhereFind, findDoc, 10, returnFields);
                 if (scrollResp2.getHits().totalHits() != 0) {
                     System.out.println("EQUALS DOCS FOUND: " + scrollResp2.getHits().totalHits());
-                    deleteFromES(findDoc, indexWhereFind);
-                    deleteCounter = deleteCounter + scrollResp2.getHits().totalHits();
+                    //deleteFromES(findDoc, indexWhereFind);
+                    deleteCounter.add(scrollResp2.getHits().totalHits());
 //                    while (true) {
 //                        for (SearchHit hit2 : scrollResp2.getHits().getHits()) {
 //                            System.out.println("Index: " + hit2.getIndex());
@@ -67,7 +72,8 @@ public class FindDublicates {
                 } else {
                     System.out.println(hostToFind + " UNIQUE");
                 }
-                System.out.println("Processed: " + ++counter + " Deleted: " + deleteCounter);
+                counter.increment();
+                System.out.println("Processed: " + counter + " Deleted: " + deleteCounter);
             }
             scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(600000)).execute().actionGet();
             if (scrollResp.getHits().getHits().length == 0) {
@@ -77,15 +83,37 @@ public class FindDublicates {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        FindDublicates findHosts = new FindDublicates();
+        counter.add(0);
+        deleteCounter.add(0);
+        Integer[] reader_id = {2,3,4,5};
         String[] index = {"marketing008"};
         String[] indexWhereFind = {"marketing001", "marketing002", "marketing003", "marketing004", "marketing005", "marketing006", "marketing007", "marketing009"};
         node = ConnectAsNode.connectASNode();
         client = node.client();
 //        client = ConnectAsTransport.connectToEs("main-cluster", "10.32.18.31", 9303);
 //        FilteredQueryBuilder searchFor = filteredQuery(matchAllQuery(), andFilter(termFilter("scanMode", 0), termFilter("domain", "cetrom.net")));
-        FilteredQueryBuilder searchFor = filteredQuery(matchAllQuery(), andFilter(termFilter("scanMode", 0), termFilter("reader_id", 1)));
-        findHosts.equalHostSearch(searchFor, index, indexWhereFind);
+        List<Thread> threads = new ArrayList<>();
+        for (Integer readers_id : reader_id) {
+            Thread find = new Thread() {
+                public void run() {
+                    setName("Tread number by id: " + readers_id);
+                    FindDublicates findHosts = new FindDublicates();
+                    try {
+                        FilteredQueryBuilder searchFor = filteredQuery(matchAllQuery(), andFilter(termFilter("scanMode", 0), termFilter("reader_id", readers_id)));
+                        findHosts.equalHostSearch(searchFor, index, indexWhereFind, readers_id);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " finished");
+                }
+            };
+            find.setDaemon(true);
+            find.start();
+            threads.add(find);
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
         Thread.sleep(5000);
         client.close();
         node.close();
